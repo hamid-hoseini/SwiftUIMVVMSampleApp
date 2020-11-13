d9204b68f3da2e7a69aad91dc4205d871a64a54c
Hamid <mail.hamid.hoseini@gmail.com>
2020-11-12 22:40:55 -0800

    first commit

--- SwiftUIMVVMSampleApp/ContentView.swift
+++ SwiftUIMVVMSampleApp/ContentView.swift
@@ -7,10 +7,39 @@
 
 import SwiftUI
 
+struct ToDoListItem: Identifiable {
+    var id = UUID()
+    var action: String
+}
+
+class ToDoList: ObservableObject {
+    @Published var items: [ToDoListItem] = []
+}
+
 struct ContentView: View {
+    @ObservedObject var viewModel: ToDoList = ToDoList()
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                List(viewModel.items) { item in
+                    Text(item.action)
+                }
+            }
+            .navigationTitle("To Do List")
+            .navigationBarItems(trailing: Button(action: {
+                
+                self.viewModel.items = [
+                    ToDoListItem(action: "Go Running"),
+                    ToDoListItem(action: "Get Milk"),
+                    ToDoListItem(action: "Do Work"),
+                    ToDoListItem(action: "Upload Video"),
+                    ToDoListItem(action: "Teach SwiftUI")
+                ]
+                
+            }, label: {
+                Text("Refresh")
+            }))
+        }
     }
 }
 
